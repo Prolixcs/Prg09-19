372873642ec01a5f90663d8aff37aad8586390ac
Lucas Andreasson <lucas.andreasson@elev.ga.lbs.se>
2018-09-19 14:37:44 +0200

    init Unity

---
+++ Assets/Scripts/Usable.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Usable : Item
+{
+    private int uses;
+
+    public int maxUses;
+    public string usetext;
+    public string depletedText;
+
+    public void Start()
+    {
+        uses = maxUses;
+    }
+
+
+    private void OnMouseUpAsButton()
+    {
+        if (uses > 0)
+        {
+            Debug.Log(ToString());
+            uses--;
+        }
+        else
+            Debug.Log(depletedText);
+    }
+
+    public virtual void Use()
+    {
+        Debug.Log(usetext);
+    }
+
+    //public virtual string UseText()
+    //{
+
+    //}
+
+    public override string ToString()
+    {
+        Use();
+        return displayName + " has " + uses + " out of " + maxUses + "uses";
+    }
+
+
+}
